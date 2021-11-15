3d66466e00b0701767fbf8e1d7c27c6393cb399c
Alena Sidel <sidelnikova.alena@gmail.com>
2021-11-15 11:50:21 +0500

    Update solution2.sql

--- hackerrank/Top_Competitors/solution2.sql
+++ hackerrank/Top_Competitors/solution2.sql
@@ -8,7 +8,8 @@ on s.challenge_id = c.challenge_id
 on c.difficulty_level = d.difficulty_level 
      inner join hackers h
 on s.hacker_id = h.hacker_id
-Where s.score = d.score and c.difficulty_level = d.difficulty_level
+Where s.score = d.score and 
+      c.difficulty_level = d.difficulty_level
 Group by h.hacker_id, h.name
 Having count(s.hacker_id) > 1
 Order by count(s.hacker_id) DESC, s.hacker_id ASC
